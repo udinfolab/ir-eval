05d295767ba24e9d191b583fa93cb95f02d73a9d
Xitong Liu <foremire@gmail.com>
2014-05-05 23:13:26 -0400

    finished merging with student retrieval results

--- import/merge-ret.pl
+++ import/merge-ret.pl
@@ -2,10 +2,12 @@
 
 #
 # Merge the retrieval results (TREC format) from multiple runs
+# * baselines
+# * student submitted retrieval functions
 #
 # Version: 1.0.
 #
-# Usage: merge-ret.pl [-v] <bl_dir> <save>\n";
+# Usage: merge-ret.pl [-v] <bl_dir> <stu_dir> <save>\n";
 #        -v: verbose mode (default: no)
 #
 # HISTORY
@@ -16,7 +18,7 @@ use strict;
 use POSIX;
 use Getopt::Long;
 
-my $usage = "merge-ret.pl [-v] <bl_dir> <save>\n";
+my $usage = "merge-ret.pl [-v] <bl_dir> <stu_dir> <save>\n";
 my $verbose = 0;
 my $rp = 0;
 
@@ -27,11 +29,12 @@ GetOptions('verbose!' => \$verbose,
 #@ARGV = map { /.gz$/ ? "gzip -dc $_ |" : $_ } @ARGV;
 
 my $bl_dir = shift or die $usage;
+my $stu_dir = shift or die $usage;
 my $save_file = shift or die $usage;
 
 # global statical variables
 my $BL_RANK_THRED = 50;
-my $STUDENT_RANK_THRED = 10;
+my $STU_RANK_THRED = 10;
 
 # global variables
 my %ret;
@@ -46,6 +49,7 @@ sub main(){
     return;
   }
   load_bl_ret_list();
+  load_stu_ret_list();
   save_merged();
 }
 
@@ -59,12 +63,26 @@ sub load_bl_ret_list(){
   my $so_far = 0;
   for my $file (sort {$a cmp $b} @files){
     my $ret_file = "$bl_dir/$file";
-    load_ret_file($ret_file, $BL_RANK_THRED);
+    load_bl_ret_file($ret_file, $BL_RANK_THRED);
   }
 }
 
-sub load_ret_file(){
-  my $usage = "Usage: load_ret_file(\$ret_file, \$thred)\n";
+sub load_stu_ret_list(){
+  # open the directory of retrieval results generated from students
+  opendir(my $dh, $stu_dir) or die "Can not opendir `$stu_dir': $!\n";
+  my @files = grep { !/^\./ && -f "$stu_dir/$_" } readdir($dh);
+  closedir $dh;
+  print @files . " files in total in $stu_dir\n";
+
+  my $so_far = 0;
+  for my $file (sort {$a cmp $b} @files){
+    my $ret_file = "$stu_dir/$file";
+    load_stu_ret_file($ret_file, $STU_RANK_THRED);
+  }
+}
+
+sub load_bl_ret_file(){
+  my $usage = "Usage: load_bl_ret_file(\$ret_file, \$thred)\n";
   my $ret_file = shift;
   my $thred = shift;
   
@@ -80,6 +98,30 @@ sub load_ret_file(){
     next if /^$/;
 
     my($qid, undef, $did, $rank, $score, $tag) = split;
+    next if $rank > $thred;
+    $ret{$qid}{$did} += 1;
+  }
+
+  close RET;
+}
+
+sub load_stu_ret_file(){
+  my $usage = "Usage: load_stu_ret_file(\$ret_file, \$thred)\n";
+  my $ret_file = shift;
+  my $thred = shift;
+  
+  unless(defined $ret_file and defined $thred){
+    die $usage;
+  }
+
+  open RET, $ret_file or die "Can not open `$ret_file': $!\n";
+  print "Loading $ret_file\n";
+
+  while(<RET>){
+    chomp;
+    next if /^$/;
+
+    my($qid, $did, $rank, $score) = split;
     next if $rank > $thred;
     $ret{$qid}{$did} += 1;
   }
